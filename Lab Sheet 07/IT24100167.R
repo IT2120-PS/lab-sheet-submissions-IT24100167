4f2340d5a89bd713093c40ceef10d36e1117f012
IT24100167 <it24100167@my.sliit.lk>
2025-09-16 23:43:05 +0530

    Lab sheet 7

---
+++ Lab Sheet 07/IT24100167.R
@@ -0,0 +1,16 @@
+#Exercise
+setwd("C:\\Users\\thisu\\OneDrive\\Desktop\\IT24100167")
+
+
+#Q1
+punif(25, min=0, max=40,lower.tail=TRUE)-punif(10, min=0, max=40, lower.tail=TRUE)
+
+#Q2
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+#Q3
+#i)
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+
+#ii)
+qnorm(0.95, mean=100, sd=15, lower.tail=TRUE)
